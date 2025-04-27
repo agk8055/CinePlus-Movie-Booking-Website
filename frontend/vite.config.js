8e81338d4b070fbe9014320dc5d221f6c4ccc953
agk8055 <alangeokurian10@gmail.com>
2025-04-27 16:31:27 +0530

    make frontend production ready

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'mui-vendor': ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          'qr-vendor': ['html5-qrcode', 'react-qr-code', 'react-qr-reader'],
+          'utils-vendor': ['axios', 'moment-timezone', 'react-toastify'],
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 5173,
+    host: true,
+  },
 })
